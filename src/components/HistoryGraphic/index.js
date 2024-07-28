3ac071583e6180412b53d3bb72364ed14fdf1e5a
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-28 02:37:08 -0300

    Histórico de Cotações por Linechart

---
+++ src/components/HistoryGraphic/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {View, Dimensions } from 'react-native'
+import { LineChart } from 'react-native-chart-kit'
+
+export default function HistoryGraphic(props){
+
+  return (
+
+    <View>
+
+      <LineChart
+        data={{
+          datasets: [
+          {
+            data: props.infoDataGraphic
+          }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        withVerticalLines={false}
+        yLabelsOffset={1}
+        withVerticalLabels={false}
+        chartConfig={{
+          backgroundColor: "#000000",
+          backgroundGradientFrom: "#232323",
+          backgroundGradientTo: "#3F3F3F",
+          decimalPlaces: 0, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          propsForDots: {
+            r: "1",
+            strokeWidth: "1",
+            stroke: "#f50d41",
+          },
+        }}
+        bezier//ondas suaves do grafico
+      />
+
+    </View>
+
+  )
+
+}
