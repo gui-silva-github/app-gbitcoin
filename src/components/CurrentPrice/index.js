d0bb7173595f91891726e4de493dae1f571039b8
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-07-28 02:36:17 -0300

    Atualizando o preço atual de maneira dinâmica

---
+++ src/components/CurrentPrice/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import styles from './styles'
+
+export default function CurrentPrice(props){
+
+  return (
+
+    <View style={styles.headerPrice}>
+      <Text style={styles.textPriceTitle}>
+        Cotação atual:
+      </Text>
+      <Text style={styles.currentPrice}>
+        $ { props.lastCotation }
+      </Text>
+      <Text style={styles.textPrice}>
+        Ultimas cotações:
+      </Text>
+    </View>
+
+  )
+
+}
---
+++ src/components/CurrentPrice/styles.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  headerPrice: {
+    width: "100%",
+    height: "auto",
+    alignItems: "center",
+    marginBottom: 20
+  },
+  currentPrice: {
+    color: "#f50d41",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginVertical: 20
+  },
+  textPrice: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  textPriceTitle: {
+        color: '#ffffff',
+        fontSize: 22,
+        fontWeight: 'bold',
+  },
+})
+
+export default styles
